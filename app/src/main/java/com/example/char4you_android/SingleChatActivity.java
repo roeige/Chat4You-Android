0a07e73b2f64b7d1bc8eb433eb6aafa048dedd2e
Amit Cohen <amit.cohen@algosec.com>
2022-06-11 16:57:13 +0300

    added single chat activity

---
+++ app/src/main/java/com/example/char4you_android/SingleChatActivity.java
@@ -0,0 +1,14 @@
+package com.example.char4you_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_chat);
+    }
+}
